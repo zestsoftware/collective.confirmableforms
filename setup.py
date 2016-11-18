1b808aafe372c969606f07c5eb35fdd6dc46e570
Maurits van Rees <maurits@vanrees.org>
2016-11-18 16:59:51 +0100

    Improved setup.py

--- setup.py
+++ setup.py
@@ -2,36 +2,38 @@ from setuptools import setup, find_packages
 
 version = '1.4.3.dev0'
 
-setup(name='collective.confirmableforms',
-      version=version,
-      description="A Plone add on to simplify form confirmation by e-mail.",
-      long_description=(open("README.rst").read() + "\n" +
-                        open("CHANGES.rst").read()),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
-          "Framework :: Plone",
-          "Programming Language :: Python",
-          ],
-      keywords='',
-      author='Vincent Pretre',
-      author_email='v.pretre@zestsoftware.nl',
-      url='https://github.com/zestsoftware/collective.confirmableforms',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-          'Products.PloneFormGen',
-          'collective.depositbox',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
+setup(
+    name='collective.confirmableforms',
+    version=version,
+    description="A Plone add on to simplify form confirmation by e-mail.",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.rst").read()),
+    # Get more strings from
+    # https://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='PloneFormGen confirmation email',
+    author='Vincent Pretre',
+    author_email='v.pretre@zestsoftware.nl',
+    url='https://github.com/zestsoftware/collective.confirmableforms',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Products.PloneFormGen',
+        'collective.depositbox',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
 
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
